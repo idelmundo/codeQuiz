d04570c4d8cb4e7396bc6c73c412822f4b6bc742
Isaias Del Mundo <idelmundo68@gmail.com>
2020-06-22 20:02:07 -0700

    add more things on j's

---
+++ script.js
@@ -0,0 +1,125 @@
+//Create HTML with header, h1, button and timmer.
+// event handler, while loop
+// We will need to make a unique id for each items.
+// Make 5 questions about anything 
+// record high scores and name. 
+
+var quizBox = document.getElementById("quiz");
+var submitButton = document.getElementById("submit");
+var highscorebox = document.getElementById("result");
+var timer = document.getElementById("time");
+var startOfquestion = document.getElementById("question");
+var answersOfQuestion = document.getElementById("answers");
+var counter = 0; //move to the next question
+var time = 15; //give 15sec total time game
+var score = 0;
+var intialTime; // helps clear interval stop timer
+
+function start() {
+    initialTime = setInterval(gameTime, 1000);
+    nextQuestion();
+}
+
+function compareAnswer() {
+    if (this.textContent === myQuestions[counter].correctAnswer) {
+        score++
+    } else {
+        time -= 2;
+    }
+    counter++
+    nextQuestion()
+}
+
+function nextQuestion() {
+    // clear old qs 
+    if (counter <= 4) {
+
+
+        startOfquestion.textContent = myQuestions[counter].question
+        answersOfQuestion.innerHTML = "";
+        myQuestions[counter].answer.forEach(element => {
+            var answerButton = document.createElement("button");
+            answerButton.textContent = element;
+            answerButton.onclick = compareAnswer;
+            answersOfQuestion.appendChild(answerButton);
+        });
+    } else {
+        endGame();
+    }
+    // increase time
+    // show array qs and answer
+
+}
+
+
+function gameTime() {
+    time--;
+    timer.textContent = time;
+    if (time <= 0) {
+        endGame()
+    }
+}
+// endGame only clears questions.
+function endGame() {
+    clearInterval(initialTime);
+    var highscorebox = document.createElement("div")
+        // clear questions innerhtml/hide/delete div
+
+
+}
+
+// Question starts here
+const myQuestions = [{
+        question: "What car did Tesla produce first?",
+        answer: [
+            "Model S",
+            "Model X",
+            "Model Y",
+            "Roadster"
+        ],
+        correctAnswer: "Roadster"
+    },
+    {
+        question: "What type of body style is Rivian making?",
+        answer: [
+            "Coupe",
+            "Truck",
+            "Sedan",
+            "Hatch"
+        ],
+        correctAnswer: "Truck"
+    },
+    {
+        question: "What is cruise trying to establish?",
+        answer: [
+            "Automotive",
+            "Robot",
+            "Autopilot",
+            "Self-driving vehicle service"
+        ],
+        correctAnswer: "Self-driving vehicle service"
+    },
+    {
+        question: "What is cruise trying to establish?",
+        answer: [
+            "Automotive",
+            "Robot",
+            "Autopilot",
+            "Self-driving vehicle service"
+        ],
+        correctAnswer: "Self-driving vehicle service"
+    },
+    {
+        question: "How much is tesla net worth",
+        answer: [
+            "$15 billion",
+            "$24.6 billion",
+            "$10 billion",
+            "$25 million"
+        ],
+        correctAnswer: "$24.6 billion"
+    },
+]
+
+// submit button 
+submitButton.addEventListener('click', start);
